224adf07dfcc23f1a8abb877720bda81a5b2378d
zeratul117 <luisbelis@hotmail.com>
2019-08-30 12:03:11 -0400

    Adding the basic navbar to proyect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
+import NavBar from './components/layout/Navbar'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>MarioPlan</h1>
+        <NavBar />
       </div>
     </BrowserRouter>
   );
