121f402a3d96ba8a4b2ac27f79a81bdab8dcb328
zeratul117 <luisbelis@hotmail.com>
2019-08-30 14:21:07 -0400

    Added the dashboard to the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import NavBar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <NavBar />
+        <Switch>
+          <Route path='/' component={Dashboard}/>
+        </Switch>
       </div>
     </BrowserRouter>
   );
